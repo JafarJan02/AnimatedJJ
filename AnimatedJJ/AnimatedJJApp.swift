4de6e3be32b4f3dacc8993d97d17727d178d66a0
Jafar Janshanlo <jafar.janshanlo20@gmail.com>
2024-03-28 10:43:14 +0300

    Initial Commit

---
+++ AnimatedJJ/AnimatedJJApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedJJApp.swift
+//  AnimatedJJ
+//
+//  Created by Jafar on 28.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedJJApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
